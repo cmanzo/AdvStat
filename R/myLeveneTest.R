589f3401eea0dd5ca83e3becb33604732f8d0be4
Carlo <carlo.manzo@gmail.com>
2021-10-28 10:51:28 +0200

    fixed error

--- R/myLeveneTest.R
+++ R/myLeveneTest.R
@@ -1,12 +1,12 @@
 myLeveneTest <- function(f,data){
-
-  if(length(f)==2){
+x<-data[,all.vars(f)[1]]
+  if(length(all.vars(f))==2){
     g<-data[,all.vars(f)[2]]
     gp.median <- tapply(x, g, median)  # calculate group means
     xbar <- gp.median[g] # group mean associated with each obs
     z <- abs(x - xbar) # the transformed variable, Z
     a<- aov(z ~ g)
-  } else if (length(f)==3) {
+  } else if (length(all.vars(f))==3) {
     g1<-data[,all.vars(f)[2]]
     g2<-data[,all.vars(f)[3]]
     g3<-interaction(g1, g2)
